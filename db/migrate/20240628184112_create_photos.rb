bc96157b15bd5e58acc3b55b644761e5d93078f9
AdolfoNava <adolfonava1990@gmail.com>
2024-06-28 13:48:46 -0500

    Generated scaffold of photos

---
+++ db/migrate/20240628184112_create_photos.rb
@@ -0,0 +1,15 @@
+class CreatePhotos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :photos do |t|
+      t.string :image
+      t.integer :comments_count, :default => 0
+      #Ex:- :default =>''
+      t.integer :likes_count, :default => 0
+      #Ex:- :default =>''
+      t.text :caption
+      t.references :owner, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
