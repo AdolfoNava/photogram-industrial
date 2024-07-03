e743b01a0c7f5d856baa563cc11d668fd9ada8fc
AdolfoNava <adolfonava1990@gmail.com>
2024-07-03 13:27:34 -0500

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
   resources :likes
   resources :follow_requests
   resources :comments
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "photos#index"
+  get "/:username" => "users#show", as: :user
 end
